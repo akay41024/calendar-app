7d1a1a6977d49143e886f23f2dfe8e29912e09e3
Aasif Khan <akay41024@gmail.com>
2024-08-17 04:57:05 +0530

    Pushing the code from local to remote

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calendar from './components/Calendar'
+import './index.css'
+
+const App = () => {
+  return (
+    <div className='conatiner'>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
